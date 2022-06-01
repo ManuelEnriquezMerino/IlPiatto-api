f24cad5a8022b029740a13e0b16097def41b1d34
ManuelEnriquezMerino <manuel_em7@hotmail.com>
2022-06-01 19:20:11 -0300

    Modificado postPedido
    * Agregadas funciones auxiliares
    * Inserta correctamente en Pedidos y Opcionales_Pedidos_Platos
    
    Pendiente:
    * Respones
    * Control de errores

--- src/controllers/pedido.controller.js
+++ src/controllers/pedido.controller.js
@@ -14,62 +14,95 @@ controller.getPedido = async(req,res) => {
     }
 }
 
-controller.postPedido = async(req,res) => {
-    var precio = 0
-    //const correo = req.auth.payload['https://ilpiatto.com/email']
-    //const idUsuario = await pool.query('SELECT id FROM usuarios WHERE correo=$1;', [correo]) //Migracion no creada?
-    var body = req.body
-    var obtenerPrecios = 'SELECT precio FROM platos WHERE id IN ('
-    var obtenerPreciosOpcionales = 'SELECT precio FROM opcionales WHERE id IN ('
-    var insertar = 'INSERT INTO opcional_pedido_plato (plato_id, opcional_id, pedido_id, N_orden) values '
 
-    for(var i=1; i<req.body.length; i++){
-        obtenerPrecios+=req.body[i].plato+','
-        console.log(req.body[i].opcionales)
-        if(req.body[i].opcionales.length>0)
-            for(var j=0; j<req.body[i].opcionales.length; j++){
-                obtenerPreciosOpcionales+=req.body[i].opcionales[j]+','
-                insertar+=`(${req.body[i].plato} , ${req.body[i].opcionales[j]}, IDPEDIDO, ${i}), `
-            }
+function generarInsertOpcionalPedidoPlato(body){
+    var insertarPedido = 'INSERT INTO opcional_pedido_plato (plato_id, opcional_id, pedido_id, n_orden) values '
+    for(var i=0; i<body.length; i++){
+        if(body[i].opcionales.length==0)
+            insertarPedido+=`(${body[i].plato} , NULL, IDPEDIDO, ${i}), `
         else
-            insertar+=`(${req.body[i].plato} , NULL, IDPEDIDO, ${i}), `
+            for(var j=0; j<body[i].opcionales.length; j++)
+                insertarPedido+=`(${body[i].plato} , ${body[i].opcionales[j]}, IDPEDIDO, ${i}), `
+                
     }
-    
-    obtenerPrecios = obtenerPrecios.slice(0,-1) + ')'
+    insertarPedido = insertarPedido.slice(0,-2) + ';'
+    return insertarPedido
+}
+
+function generarSelectPrecioOpcionales(body){ //VERIFICAR QUE LOS OPCIONALES PERTENECEN AL PLATO
+    var obtenerPreciosOpcionales = 'SELECT precio FROM opcionales WHERE id IN ('
+    for(var i=0; i<body.length; i++){
+        for(var j=0; j<body[i].opcionales.length; j++)
+            obtenerPreciosOpcionales+=body[i].opcionales[j]+','
+    }
     obtenerPreciosOpcionales = obtenerPreciosOpcionales.slice(0,-1) + ')'
-    insertar = insertar.slice(0,-2) + ';'
+    return obtenerPreciosOpcionales
+}
 
-    idPedido=32
+function generarSelectPrecio(body){
+    var obtenerPrecios = 'SELECT precio FROM platos WHERE id IN ('
+    for(var i=0; i<body.length; i++){
+        obtenerPrecios+=body[i].plato+','
+    }
+    obtenerPrecios = obtenerPrecios.slice(0,-1) + ')'
+    return obtenerPrecios
+}
 
-    insertar = insertar.replaceAll('IDPEDIDO',`${idPedido}`)
-
-    
-
-    console.log(insertar)
-
-    var respuestaPrecios = await pool.query(obtenerPrecios)
-
-    if(respuestaPrecios.rows.length > 0){
-        for(var k=0; k<respuestaPrecios.rows.length; k++)
-            precio += respuestaPrecios.rows[k].precio
-        var respuestaPreciosOpcionales = await pool.query(obtenerPreciosOpcionales)
+async function calcularPrecioOpcionales(obtenerPrecioOpcionales){
+    var precioOpcionales = 0
+    var respuestaPreciosOpcionales = await pool.query(obtenerPrecioOpcionales)
         if(respuestaPreciosOpcionales.rows.length > 0){
-            for(var l=0; l<respuestaPreciosOpcionales.rows.length; l++)
-                precio += respuestaPreciosOpcionales.rows[l].precio
-            console.log(precio)
-        } else {
-            //error res.status(codigo).json({error: 'texto del error'})
+            for(var i=0; i<respuestaPreciosOpcionales.rows.length; i++)
+                precioOpcionales += respuestaPreciosOpcionales.rows[i].precio
+        } 
+    return precioOpcionales
+}
+
+async function calcularPrecio(obtenerPrecio, obtenerPrecioOpcionales){
+    var precio = 0
+    var respuestaPrecios = await pool.query(obtenerPrecio)
+    if(respuestaPrecios.rows.length > 0){
+        for(var i=0; i<respuestaPrecios.rows.length; i++){
+            precio += respuestaPrecios.rows[i].precio
         }
+        precio += await calcularPrecioOpcionales(obtenerPrecioOpcionales)
     } else {
         //error res.status(codigo).json({error: 'texto del error'})
     }
-    //const respuesta = await pool.query('SELECT * FROM pedidos WHERE cliente_id;') //Falta cliente_id en la tabla de pedidos
-    //if(respuesta.rows.length > 0){
-        //res.status(200).json(respuesta.rows);
-    //} else {
-        //error res.status(codigo).json({error: 'texto del error'})
-    //}
+    return precio
+}
+
+controller.postPedido = async(req,res) => {
+    const email = req.auth.payload['https://ilpiatto.com/email']
+    const queryIDUsuario = await pool.query(`SELECT id FROM clientes WHERE email='${email}';`)
+
+    if(queryIDUsuario.rows.length > 0)
+        var idUsuario = queryIDUsuario.rows[0].id
+
+    var body = req.body
+
+    var obtenerPrecios = generarSelectPrecio(body.pedido)
+    var obtenerPreciosOpcionales = generarSelectPrecioOpcionales(body.pedido)
+    var insertarOpcionalPedidoPlato = generarInsertOpcionalPedidoPlato(body.pedido)
+
+    var precio = await calcularPrecio(obtenerPrecios,obtenerPreciosOpcionales)
+
+    const resultadoQuery = await pool.query(`INSERT INTO pedidos 
+                        (fecha,direccion,precio,cliente_id,created_at,updated_at)
+                    VALUES
+                        (current_timestamp,'${body.direccion}',${precio},${idUsuario},current_timestamp,current_timestamp)
+                    RETURNING id;`)
+
+    var idPedido = resultadoQuery.rows[0].id;
+
+    insertarOpcionalPedidoPlato = insertarOpcionalPedidoPlato.replaceAll('IDPEDIDO',`${idPedido}`)
+    
+    await pool.query(insertarOpcionalPedidoPlato)
+
     res.send("ok")
 }
+
+
+
 
 module.exports = controller
