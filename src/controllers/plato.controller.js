594ef20b8a4f16768e21b146ba0abf00d6371572
ManuelEnriquezMerino <manuel_em7@hotmail.com>
2022-06-02 16:14:33 -0300

    Modificadas rutas de plato
    * Agregadas rutas para obtener listado de categorias y restricciones
    * Agregado Swagger
    * Refinados mensajes de errores

--- src/controllers/plato.controller.js
+++ src/controllers/plato.controller.js
@@ -4,52 +4,92 @@ const pool = require('../database')
 const atributos = 'id,nombre,descripcion,precio'
 
 controller.getPlato = async(req,res) => {
-    const respuesta = await pool.query(`SELECT ${atributos} FROM platos;`) //Enviar fecha de creacion y actualizacion??
-    if(respuesta.rows.length > 0){
-        res.status(200).json(respuesta.rows);
-    } else {
-        res.status(404).json({error: 'No hay platos disponibles'})
+    try{
+        const respuesta = await pool.query(`SELECT ${atributos} FROM platos;`)
+        if(respuesta.rows.length > 0){
+            res.status(200).json(respuesta.rows);
+        } else {
+            res.status(404).json({error: 'No hay platos disponibles'})
+        }
+    } catch (err) {
+        res.status(500).json({error: 'Error en el servidor'})
     }
 }
 
 controller.getPlatoID = async(req,res) => {
-    const id = req.params.id;
+    try{
+        const id = req.params.id;
 
-    if(!isNaN(id)){
-        const respuesta = await pool.query(`SELECT ${atributos} FROM platos WHERE id=${id};`)
-        if(respuesta.rows.length > 0)
-            res.status(200).json(respuesta.rows);
-        else
-            res.status(404).json({error: 'El plato ingresado no existe'})
-    } else
-        res.status(404).json({error: 'El plato debe ser ingresado como un entero'})  
-    
+        if(!isNaN(id)){
+            const respuesta = await pool.query(`SELECT ${atributos} FROM platos WHERE id=${id};`)
+            if(respuesta.rows.length > 0)
+                res.status(200).json(respuesta.rows);
+            else
+                res.status(404).json({error: 'El plato ingresado no existe'})
+        } else
+            res.status(400).json({error: 'El plato debe ser ingresado como un entero'})  
+    } catch (err) {
+        res.status(500).json({error: 'Error en el servidor'})
+    }
 }
 
 controller.getPlatoCategoria = async(req,res) => {
-    const idCategoria = req.params.idCategoria;
+    try{
+        const idCategoria = req.params.idCategoria;
 
-    if(!isNaN(idCategoria)){
-        const respuesta = await pool.query(`SELECT ${atributos} FROM platos WHERE id in (SELECT plato_id FROM categoria_plato where categoria_id=${idCategoria})`)
-        if(respuesta.rows.length > 0)
-            res.status(200).json(respuesta.rows);
-        else
-            res.status(404).json({error: 'La categoria ingresada no existe'})
-    } else
-        res.status(404).json({error: 'La categoria debe ser ingresada como un entero'})
+        if(!isNaN(idCategoria)){
+            const respuesta = await pool.query(`SELECT ${atributos} FROM platos WHERE id in (SELECT plato_id FROM categoria_plato where categoria_id=${idCategoria});`)
+            if(respuesta.rows.length > 0)
+                res.status(200).json(respuesta.rows);
+            else
+                res.status(404).json({error: 'La categoria ingresada no existe'})
+        } else
+            res.status(400).json({error: 'La categoria debe ser ingresada como un entero'})
+    } catch (err) {
+        res.status(500).json({error: 'Error en el servidor'})
+    }
 }
 
 controller.getPlatoRestriccion = async(req,res) => {
-    const idRestriccion = req.params.idRestriccion;
+    try{
+        const idRestriccion = req.params.idRestriccion;
 
-    if(!isNaN(idRestriccion)){
-        const respuesta = await pool.query(`SELECT ${atributos} FROM platos WHERE id in (SELECT plato_id FROM plato_restriccion where restriccion_id=${idRestriccion})`)
-        if(respuesta.rows.length > 0)
-            res.status(200).json(respuesta.rows);
-        else
-            res.status(404).json({error: 'La restriccion ingresada no existe'})
-    } else
-        res.status(404).json({error: 'La restriccion debe ser ingresada como un entero'})  
+        if(!isNaN(idRestriccion)){
+            const respuesta = await pool.query(`SELECT ${atributos} FROM platos WHERE id in (SELECT plato_id FROM plato_restriccion where restriccion_id=${idRestriccion});`)
+            if(respuesta.rows.length > 0)
+                res.status(200).json(respuesta.rows);
+            else
+                res.status(404).json({error: 'La restriccion ingresada no existe'})
+        } else
+            res.status(400).json({error: 'La restriccion debe ser ingresada como un entero'})  
+    } catch (err) {
+        res.status(500).json({error: 'Error en el servidor'})
+    }
 }
+
+controller.getCategorias = async(req,res) => {
+    try{
+        const respuesta = await pool.query(`SELECT id,nombre,descripcion FROM categorias;`)
+            if(respuesta.rows.length > 0)
+                res.status(200).json(respuesta.rows);
+            else
+                res.status(404).json({error: 'No hay categorias disponibles'})
+    } catch (err) {
+        res.status(500).json({error: 'Error en el servidor'})
+    }
+}
+
+controller.getRestricciones = async(req,res) => {
+    try{
+        const respuesta = await pool.query(`SELECT id,nombre,descripcion FROM restricciones;`)
+            if(respuesta.rows.length > 0)
+                res.status(200).json(respuesta.rows);
+            else
+                res.status(404).json({error: 'No hay restricciones disponibles'})
+    } catch (err) {
+        res.status(500).json({error: 'Error en el servidor'})
+    }
+}
+
 
 module.exports = controller
--- src/routes/plato.routes.js
+++ src/routes/plato.routes.js
@@ -2,9 +2,204 @@ const express = require('express')
 const router = express.Router()
 const PlatoController = require('../controllers/plato.controller')
 
+/**
+*  @swagger
+*   components:
+*     schemas:
+*       Plato:
+*         type: object
+*         properties:
+*          id:
+*            type: int
+*            description: ID auto generado para el usuario.
+*            example: 1
+*          nombre:
+*            type: string
+*            description: Nombre del plato.
+*            example: Tallarines con salsa bolognesa
+*          descripcion:
+*            type: string
+*            description: Descripcion del plato.
+*            example: Tallarines hechos a mano con salsa bolognesa a base de ingredientes organicos
+*          precio:
+*            type: int
+*            description: Precio del plato.
+*            example: 1500
+*/
+
+
+/**
+*  @swagger
+*  tags:
+*    name: Plato
+*    description: API para acceder a los datos de los platos.
+*/
+
+/**
+*   @swagger
+*   /plato/:
+*       get:
+*           summary: Obtiene un listado de todos los platos.
+*           tags: [Plato]
+*           responses:
+*               "200":
+*                   description: Listado de platos.
+*                   content:
+*                       application/json:
+*                           schema:
+*                               $ref: '#/components/schemas/Plato'
+*               "404":
+*                   description: No hay platos disponibles
+*               "500":
+*                   description: Error en el servidor
+*/
 router.get('/',PlatoController.getPlato)
+
+/**
+*   @swagger
+*   /plato/categoria/:
+*       get:
+*           summary: Obtiene un listado de todas las categorias.
+*           tags: [Plato]
+*           responses:
+*               "200":
+*                   description: Listado de categorias.
+*                   content:
+*                       application/json:
+*                           schema:
+*                                 type: object
+*                                 properties:
+*                                   id:
+*                                       type: int
+*                                       example: 1
+*                                   nombre:
+*                                       type: string
+*                                       example: Pastas
+*                                   descripcion:
+*                                       type: string
+*                                       example:  Platos basados en Pastas
+*               "404":
+*                   description: No hay categorias disponibles
+*               "500":
+*                   description: Error en el servidor
+*/
+router.get('/categoria',PlatoController.getCategorias)
+
+/**
+*   @swagger
+*   /plato/restriccion/:
+*       get:
+*           summary: Obtiene un listado de todas las restricciones.
+*           tags: [Plato]
+*           responses:
+*               "200":
+*                   description: Listado de restricciones.
+*                   content:
+*                       application/json:
+*                           schema:
+*                                 type: object
+*                                 properties:
+*                                   id:
+*                                       type: int
+*                                       example: 1
+*                                   nombre:
+*                                       type: string
+*                                       example: Celiaco
+*                                   descripcion:
+*                                       type: string
+*                                       example: Platos que solo contienen alimentos sin TACC
+*               "404":
+*                   description: No hay restricciones disponibles
+*               "500":
+*                   description: Error en el servidor
+*/
+router.get('/restriccion',PlatoController.getRestricciones)
+
+/**
+*   @swagger
+*   /plato/{id}:
+*       get:
+*           summary: Obtiene los datos de un dado plato.
+*           tags: [Plato]
+*           parameters:
+*             - in: path
+*               name: id
+*               schema:
+*                   type: integer
+*                   required: true
+*                   description: Id del plato
+*           responses:
+*               "200":
+*                   description: Datos del platos.
+*                   content:
+*                       application/json:
+*                           schema:
+*                               $ref: '#/components/schemas/Plato'
+*               "400":
+*                   description: El plato debe ser ingresado como un entero.
+*               "404":
+*                   description: El plato ingresado no existe. 
+*               "500":
+*                   description: Error en el servidor
+*/
 router.get('/:id',PlatoController.getPlatoID)
+
+/**
+*   @swagger
+*   /plato/categoria/{id}:
+*       get:
+*           summary: Obtiene un listado de los platos que pertenecen a una dada categoria.
+*           tags: [Plato]
+*           parameters:
+*             - in: path
+*               name: id
+*               schema:
+*                   type: integer
+*                   required: true
+*                   description: Id de la categoria
+*           responses:
+*               "200":
+*                   description: Listado de platos.
+*                   content:
+*                       application/json:
+*                           schema:
+*                               $ref: '#/components/schemas/Plato'
+*               "400":
+*                   description: La categoria debe ser ingresada como un entero.
+*               "404":
+*                   description: La categoria ingresada no existe. 
+*               "500":
+*                   description: Error en el servidor
+*/
 router.get('/categoria/:idCategoria',PlatoController.getPlatoCategoria)
+
+/**
+*   @swagger
+*   /plato/restriccion/{id}:
+*       get:
+*           summary: Obtiene un listado de los platos que siguen una dada restriccion.
+*           tags: [Plato]
+*           parameters:
+*             - in: path
+*               name: id
+*               schema:
+*                   type: integer
+*                   required: true
+*                   description: Id de la restriccion
+*           responses:
+*               "200":
+*                   description: Listado de platos.
+*                   content:
+*                       application/json:
+*                           schema:
+*                               $ref: '#/components/schemas/Plato'
+*               "400":
+*                   description: La restriccion debe ser ingresada como un entero.
+*               "404":
+*                   description: La restriccion ingresada no existe. 
+*               "500":
+*                   description: Error en el servidor
+*/
 router.get('/restriccion/:idRestriccion',PlatoController.getPlatoRestriccion)
 
 module.exports = router
