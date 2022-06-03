c9745effebe7e8ad42723c9dd92bc559ffdb65bd
ManuelEnriquezMerino <manuel_em7@hotmail.com>
2022-06-02 22:12:40 -0300

    Cambios para subir a Heroku

--- src/swaggerOptions.js
+++ src/swaggerOptions.js
@@ -26,7 +26,7 @@ const options = {
       },
       servers: [
         {
-          url: "http://localhost:3000/", //Cambiar esto al pushearlo a heroku!!!
+          url: "http://il-piatto-api.herokuapp.com/",
         },
       ],
     },
